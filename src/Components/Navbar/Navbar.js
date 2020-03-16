fefc13f62a924388948fd0691b962a41d5d51fb3
caio_ingber <caioingber@gmail.com>
2020-03-15 23:08:35 -0400

    created Nav component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import Pdf from "../../Contents/CaioIngber_Resume.pdf";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from "reactstrap";
+
+const Example = props => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="info" dark expand="true">
+        <NavbarBrand href="/">Caio Ingber</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href={Pdf} target="_blank">
+                Resume
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/caioingber" target="_blank">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Projects
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText></NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Example;
